c6db9f7bfa2f41a4b51cdda39788d6020cd79856
Spandan <spandandec21@gmail.com>
2023-08-26 11:33:37 +0545

    Logout Feature in Dashboard Navbar

--- src/components/(admin)/AdminNavbar.jsx
+++ src/components/(admin)/AdminNavbar.jsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react'
 import AdminSidebar from '@/components/(admin)/AdminSidebar'
-import Link from 'next/link'
+import { signOut } from 'next-auth/react'
 
 const AdminNavbar = () => {
     const [showSidebar, setshowSidebar] = useState(false);
@@ -19,16 +19,16 @@ const AdminNavbar = () => {
     }
 
     return (
-        <div>
+        <>
             <div className='flex items-center justify-between p-4 bg-[#F9F9FB]'>
                 <button onClick={() => setshowSidebar(!showSidebar)} className="md:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
                 </button>
-                <button>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                <button className='ml-auto' onClick={signOut}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
                     </svg>
                 </button>
             </div>
@@ -37,7 +37,7 @@ const AdminNavbar = () => {
                     <AdminSidebar />
                 </div>
             )}
-        </div>
+        </>
     )
 }
 
--- src/components/(user)/UserNavbar.jsx
+++ src/components/(user)/UserNavbar.jsx
@@ -1,8 +1,8 @@
 "use client"
 
 import React, { useState, useEffect } from 'react'
-import Link from 'next/link'
 import UserSidebar from '@/components/(user)/UserSidebar'
+import { signOut } from 'next-auth/react'
 
 const UserNavbar = () => {
     const [showSidebar, setshowSidebar] = useState(false);
@@ -24,17 +24,18 @@ const UserNavbar = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
                 </button>
-                <button className=' flex w-full justify-end'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                <button className=' ml-auto' onClick={signOut} >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
                     </svg>
                 </button>
-            </div>
+            </div >
             {showSidebar && (
                 <div className='relative h-full z-[80] w-72'>
                     <UserSidebar />
                 </div>
-            )}
+            )
+            }
         </>
     )
 }
